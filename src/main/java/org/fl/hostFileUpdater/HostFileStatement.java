9fb71a4c411f3753b1ee8df0dad5c13bde98d829
Frederic Lefevre <frederic.lefevre@fr.ibm.com>
2019-08-05 08:29:30 +0200

    Simplify

--- src/main/java/org/fl/hostFileUpdater/HostFileStatement.java
+++ src/main/java/org/fl/hostFileUpdater/HostFileStatement.java
@@ -48,7 +48,7 @@ public class HostFileStatement {
 	// True if the IP address maps have not the same IP but have a name in common
 	public boolean containSameHostNameWithDiffentAddress(HostFileStatement hfs) {
 		
-		if (isCommentLine || hfs.isCommentLine || (ipAddressMap == null) || (hfs.getIpAddressMap() == null)) {
+		if ((ipAddressMap == null) || (hfs.getIpAddressMap() == null)) {
 			return false ;
 		} else {
 			return ipAddressMap.containSameHostNameWithDiffentAddress(hfs.getIpAddressMap()) ;
