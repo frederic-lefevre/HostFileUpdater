e92816b4435dd301d56da44865c59d67ef3a2aee
Frederic Lefevre <frederic.bn.lefevre@googlemail.com>
2025-05-25 20:44:32 +0200

    Fix assertj import

--- src/test/java/org/fl/hostFileUpdater/ControlTest.java
+++ src/test/java/org/fl/hostFileUpdater/ControlTest.java
@@ -24,7 +24,7 @@ SOFTWARE.
 
 package org.fl.hostFileUpdater;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 import org.fl.util.RunningContext;
 import org.junit.jupiter.api.Test;
